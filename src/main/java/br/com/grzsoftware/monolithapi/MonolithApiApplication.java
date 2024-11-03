91e6255b8f28bd6b7adde59d5ddd606e1aa7d744
isacgrizante <isacgrizante18@gmail.com>
2024-11-03 13:10:22 -0300

    Initial commit

---
+++ src/main/java/br/com/grzsoftware/monolithapi/MonolithApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.grzsoftware.monolithapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/grzsoftware/monolithapi/MonolithApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grzsoftware.monolithapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
